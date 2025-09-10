a219b34d23dd46ffb307329e3efb3300099f6c7e
omidsdgi <omid69sdgi@gmail.com>
2025-09-10 09:58:44 +0330

    fixed screen height

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,8 @@ import ThemeToggle from "@/components/themeMode/ThemeToggle";
 
 export default function Home() {
   return(
-      <main  className="min-h-screen bg-white text-black dark:bg-gray-900 dark:text-white">
-          <div className={'flex flex-col justify-center items-center h-[100vh] '}>
+      <main  className="min-h-screen bg-white text-black  dark:bg-gray-900 dark:text-white">
+          <div className={'flex flex-col justify-end items-center py-20 '}>
               <ThemeToggle/>
               <Weather city={'tehran'}/>
           </div>
